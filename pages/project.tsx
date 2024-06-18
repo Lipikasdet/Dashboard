f80901db429add68a2739a93612f4cd0c337bbe1
Lipikasdet <lipika.karn@sdettechnologies.com>
2024-06-18 10:51:03 +0530

    kpi API and ui rendering

---
+++ pages/project.tsx
@@ -0,0 +1,11 @@
+import Dashboard from '../src/projectData'
+
+const ProjectPage = () => {
+   return (
+    <div>
+        <h1>Project Page</h1>
+        <Dashboard />
+    </div>
+   )
+}
+export default ProjectPage;
